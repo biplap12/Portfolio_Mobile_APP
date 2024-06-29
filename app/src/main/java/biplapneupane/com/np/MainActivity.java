c783f9942bd72574702cb458b0431a181e07c0d8
biplap <biplap.neupane2020@gmail.com>
2024-06-29 14:27:13 +0545

    first commit

---
+++ app/src/main/java/biplapneupane/com/np/MainActivity.java
@@ -0,0 +1,22 @@
+package biplapneupane.com.np;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWeb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWeb = findViewById(R.id.myWeb);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+        myWeb.setWebViewClient(new WebViewClient());
+        myWeb.loadUrl("https://biplapneupane.com.np/");
+    }
+}
